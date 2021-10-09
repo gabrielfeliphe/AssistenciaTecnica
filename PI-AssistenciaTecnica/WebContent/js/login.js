1a16aa3f03349d9734720f259e67b497bed56bbd
Gabriel <62681139+gabrielfeliphe@users.noreply.github.com>
2021-10-08 21:01:04 -0300

    .

---
+++ PI-AssistenciaTecnica/WebContent/js/login.js
@@ -0,0 +1,20 @@
+
+// revelar senha para o usuário;
+
+const togglePassword = document.querySelector('#togglePassword');
+const password = document.querySelector('#password');
+
+togglePassword.addEventListener('click', function (e) {
+  
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    this.classList.toggle('bi-eye');
+});
+
+// ----
+function onlyNumbers(matricula) {
+    var newVal = document.getElementByName(matricula).value;
+    if (isNaN(newVal)) {
+        document.getElementByName(matricula).value = newVal.replace(/[^0-9.]/g, "");
+    }
+}
