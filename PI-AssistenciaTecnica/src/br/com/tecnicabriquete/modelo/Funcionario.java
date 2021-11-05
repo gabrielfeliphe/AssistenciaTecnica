7c924108c4a252370d961be3ccd4cfd805b25654
Gabriel <62681139+gabrielfeliphe@users.noreply.github.com>
2021-11-05 12:50:48 -0300

    .

--- PI-AssistenciaTecnica/src/br/com/tecnicabriquete/modelo/Funcionario.java
+++ PI-AssistenciaTecnica/src/br/com/tecnicabriquete/modelo/Funcionario.java
@@ -1,5 +1,40 @@
 package br.com.tecnicabriquete.modelo;
 
-public class Funcionario {
+import java.io.Serializable;
+
+public class Funcionario implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private int matricula;
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getFuncao() {
+		return funcao;
+	}
+	public void setFuncao(int funcao) {
+		this.funcao = funcao;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	private String email;
+	private int funcao;
+	private String senha;
+	
+	
 
 }
