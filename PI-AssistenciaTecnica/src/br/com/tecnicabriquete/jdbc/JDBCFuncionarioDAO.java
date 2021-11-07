821b3568b0ec7f24af303c584a7a36e74db75160
Gabriel <62681139+gabrielfeliphe@users.noreply.github.com>
2021-11-07 07:25:29 -0300

    backend 1/2
    dados voltam da backend e do banco de dados, falta pegar os dados do formulário e criar novos funcionarios

--- PI-AssistenciaTecnica/src/br/com/tecnicabriquete/jdbc/JDBCFuncionarioDAO.java
+++ PI-AssistenciaTecnica/src/br/com/tecnicabriquete/jdbc/JDBCFuncionarioDAO.java
@@ -21,12 +21,15 @@ public class JDBCFuncionarioDAO {
 	}
 
 	public List<Funcionario> buscar() {
+		
+		System.out.println("Entrou no funcionario dao");
+		
 		//Criação da instrução SQL para busca de todas as marcas
 				String comando = "SELECT * FROM usuario";
 				
 				
 				//Criação de uma lista para armazenar cada marca encontrada
-				List<Funcionario> listMarcas = new ArrayList<Funcionario>();
+				List<Funcionario> listaFuncionario = new ArrayList<Funcionario>();
 				
 				//Criação do objeto marca com valor null (ou seja, sem instanciá-lo)
 				Funcionario funcionario = null;
@@ -47,25 +50,20 @@ public class JDBCFuncionarioDAO {
 					//Enquanto houver uma próxima linha no resultado
 					while(rs.next()) {
 						
-						//Criação de instância de classe Marca
 						funcionario = new Funcionario();
 						
-						//Recebimento dos 2 dados retornados do BD para cada linha encontrada
 						int matricula = rs.getInt("matricula");
 						String senha = rs.getString("senha");
 						int funcao = rs.getInt("funcao");
 						String email = rs.getString("email");
 						
-						//Setando no objeto marca os valores encontrados
-						f.setStatus(status);
 						
-						if(marca.getStatus() == 0) {
-							System.out.println("marca com status 0 pulada");
-						}else {
-							funcionario.setId(id); // POR QUESTÕES DE ECONOMIA DE PROCESSAMENTO VEM PRO ELSE 
-							funcionario.setNome(nome);
-							listMarcas.add(marca);
-						}
+		
+							funcionario.setEmail(email);
+							funcionario.setFuncao(funcao);
+							funcionario.setMatricula(matricula);
+							funcionario.setSenha(senha);
+							listaFuncionario.add(funcionario);
 					}
 					
 					
@@ -76,7 +74,7 @@ public class JDBCFuncionarioDAO {
 				}
 				
 				
-				return listMarcas;
+				return listaFuncionario;
 	}
 
 }
