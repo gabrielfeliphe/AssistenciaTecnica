177ba71dc086539d8eb7f9518d1d5d718e1094e2
Gabriel <62681139+gabrielfeliphe@users.noreply.github.com>
2022-02-15 19:48:38 -0300

    RF: 10 - MODAL
    SISTEMA DE MODAL JÁ PUXA OS DADOS, FALTA TRATATIVAS

--- .metadata/.plugins/org.eclipse.wst.server.core/tmp1/wtpwebapps/PI-AssistenciaTecnica/js/recepcao_consultaOS.js
+++ .metadata/.plugins/org.eclipse.wst.server.core/tmp1/wtpwebapps/PI-AssistenciaTecnica/js/recepcao_consultaOS.js
@@ -54,7 +54,7 @@ $(document).ready(function(){
 			if(listaDeOS[i].status >= 4){
 
 			tabela += "<tr id='tabelaExibeOrcamentos'>" +
-				"<td class='exibeInformacoes' onclick='BRIQUETE.orcamento.aprovarOrcamento("+listaDeOS[i].idorcamento+")'>" + statusOrcamento + "</td>" +
+				"<td class='exibeInformacoes' onclick='BRIQUETE.recepcao.aprovarOS("+listaDeOS[i].idorcamento+")'>" + statusOrcamento + "</td>" +
 				"<td>" + listaDeOS[i].cliente.nome+ "</td>" +
 				"<td>" + listaDeOS[i].equipamentoNome + "</td>" +
 				"</tr>"
@@ -69,5 +69,112 @@ $(document).ready(function(){
 	return tabela;
 
 	}
+	
+	
+
+	BRIQUETE.recepcao.aprovarOS = function(idorcamento){
+		
+		$.ajax({
+			
+			type: "GET",
+			url: BRIQUETE.PATH + "servicos/buscarOrcamentoId",
+			data: "idorcamento="+idorcamento,
+			success: function(dados) {
+				
+				//CARREGAMENTO DOS ITENS NA LABEL VIA DADOS
+				console.log("dados do idorcamento consultaos")
+				console.log(dados)
+				
+				document.getElementById('clienteNameModal').innerHTML  = dados.cliente.nome;
+				document.getElementById('equipamento').innerHTML  = dados.equipamentoNome;
+				document.getElementById('observacoes').innerHTML  = dados.observacao;
+				document.getElementById('mod-cod').innerHTML  = dados.equipamentoModeloCodigo;
+				document.getElementById('defeito').innerHTML  = dados.defeito;
+				document.getElementById('telefone').innerHTML = dados.cliente.telefone;
+				
+				if((dados.status == 2) || (dados.status == 3)){ // FAZ O CARREGAMENTO SE A ORDEM DE SERVIÇO JÁ FOI PREENCHIDA
+					
+					
+					document.getElementById('validadeOrcamento').innerHTML = dados.validade;
+					document.getElementById('observacoes').innerHTML = dados.observacao;
+					document.getElementById("tabelaValor").hidden = false;
+					
+					console.log(document.getElementById("tabelaValor").hidden)
+					
+					$("#tabela-orcamento").html(BRIQUETE.orcamento.exibirOrcamentoModal(dados.servicos));
+					
+				}else{
+				
+				}
+					
+				var modalRealizaOrcamento = { // CRIAÇÃO DA MODAL
+						title: "Orçamento",
+						height:450,
+						width:900,
+						modal: true,
+						buttons:{
+							"Cancelar": function(){
+								$(this).dialog("close");
+								//BRIQUETE.orcamento.limparFrm();	
+							},
+							"Finalizar": function(){
+								
+								
+								$.ajax({
+									type: "PUT",
+									url: BRIQUETE.PATH + "servicos/aprovarOrcamento/"+idorcamento+"/"+valorStatus,
+									success: function(msg) {
+										BRIQUETE.exibirAviso(msg);
+										BRIQUETE.orcamento.consultaOrcamento();
+										//BRIQUETE.orcamento.limparFrm();
+										$("#modalRealizaOS").dialog( "close" );
+									},
+									error: function(info) {
+										BRIQUETE.exibirAviso(info.responseText);
+									}
+								});
+							
+								
+							},
+							"Abandono":function(){
+								
+								$.ajax({
+									type: "PUT",
+									url: BRIQUETE.PATH + "servicos/aprovarOrcamento/"+idorcamento+"/"+"-1",
+									success: function(msg) {
+										BRIQUETE.exibirAviso(msg);
+										BRIQUETE.orcamento.consultaOrcamento();
+										//BRIQUETE.orcamento.limparFrm();
+										$("#modalRealizaOS").dialog( "close" );
+									},
+									error: function(info) {
+										BRIQUETE.exibirAviso(info.responseText);
+									}
+								});
+								
+							}
+						},
+						close: function(){
+							//BRIQUETE.orcamento.limparFrm();
+							$(this).dialog('close');
+						}
+				};
+				
+				$("#modalRealizaOS").dialog(modalRealizaOrcamento);
+				
+				if(dados.status !=5){ // desabilita botão quando for diferente de contatar o cliente;
+					$(":button:contains('Finalizar')").prop("disabled", true).addClass("ui-state-disabled")
+					$(":button:contains('Abandono')").prop("disabled", true).addClass("ui-state-disabled")
+				}
+				
+			},
+			error: function(info) {
+				console.log("erro : " + info);
+				//BRIQUETE.exibirAviso("Erro ao consultar os contatos: "+ info.status+" - " + info.statusText+ " - " + info.responseText);
+			},
+
+		});
+	}
+	
 	
 });
